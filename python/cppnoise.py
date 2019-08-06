179cf8d9cb81f24c5c1affdd43713df4376ec429
Yanfei Tang <yanfeit89@gmail.com>
2019-08-06 10:04:00 +0800

    update cppnoise

--- python/cppnoise.py
+++ python/cppnoise.py
@@ -1,29 +1,41 @@
+# Copyright (c) 2019 by Yanfei Tang (yanfeit89@163.com).
+# Open source software license: MIT
+
 import ctypes
 import numpy as np
 from time import time
 import matplotlib.pyplot as plt
 from numpy.ctypeslib import ndpointer
+from collections import namedtuple
+
+Shape = namedtuple('Shape', 'z y x')
+Resolution  = namedtuple('Resolution', 'z y x')
+
+shape      = Shape(0, 8, 16) # [z, y, x]
+resolution = Resolution(200, 512, 1024) # resolution in direction [z, y, x]
 
 s1 = time()
 
+# pass the dynamic library
+lib = ctypes.CDLL('./libperlinNoise.dylib')
 
-model = ctypes.CDLL('./libPerlinNoise.dylib')
+# get the 2d Perlin noise function
+perlinNoise2D = lib.perlinNoise2D
 
-PerlinNoise2D = model.perlinNoise2D
+# Need specify the types of the argument for function perlinNoise2D
+perlinNoise2D.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int)
 
+# This note is extremely useful to understand how to return a 2d array!
+# https://stackoverflow.com/questions/43013870/how-to-make-c-return-2d-array-to-python?noredirect=1&lq=1
+# We can never pass a 2d array, therefore return 1d array in C function
+perlinNoise2D.restype  = ndpointer(dtype=ctypes.c_float, shape=(resolution.y, resolution.x))
 
-PerlinNoise2D.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int)
-
-PerlinNoise2D.restype  = ndpointer(dtype=ctypes.c_float, shape=(1024, 1024))
-
-result = PerlinNoise2D(ctypes.c_int(8), 
-					ctypes.c_int(8), 
-					ctypes.c_int(1024), 
-					ctypes.c_int(1024))
+result = perlinNoise2D(ctypes.c_int(shape.x), 
+					ctypes.c_int(shape.y), 
+					ctypes.c_int(resolution.x), 
+					ctypes.c_int(resolution.y))
 
 print('cpp time consuming: ', time()-s1)
-
-np.savetxt("file2.txt", result )
 
 plt.imshow(result)
 plt.show()
